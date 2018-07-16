607b29ca123cb0a112b8f4e257716ffde8ceee80
kerncarboni <30427636+kerncarboni@users.noreply.github.com>
2018-07-16 23:24:54 +1200

    wip

--- BIT504/Assignment1/src/StudentManager.java
+++ BIT504/Assignment1/src/StudentManager.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class StudentManager
 	{
-		public static void main(String[] args)
+	public static void main(String[] args)
+		{
+		while (selectMenuOption() != 5)
 			{
-				System.out.println("awrite?");
+			displayMenu();
 			}
+		}
+
+	private static void displayMenu()
+		{
+		System.out.println("Student Report System");
+		System.out.println("---------------------");
+		System.out.println("1) Display Student Marks");
+		System.out.println("2) Display Student Grades");
+		System.out.println("3) Add new Student");
+		System.out.println("4) Remove Student");
+		System.out.println("5) Exit");
+
+
+		}
+
+	private static int selectMenuOption()
+		{
+		Scanner scanner = new Scanner(System.in);
+		int menuOption = scanner.nextInt();
+		return menuOption;
+		}
 	}
