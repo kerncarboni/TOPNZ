97fa9d56c38016dccc51273c94a4a9f33bfea306
kerncarboni <30427636+kerncarboni@users.noreply.github.com>
2018-07-22 20:22:16 +1200

    wip

---
+++ BIT504/Assignment1/src/Student.java
@@ -0,0 +1,6 @@
+public class Student
+	{
+	public Student(int id, String firstName, String lastName, int mathsMark1, int mathsMark2, int mathsMark3, int englishMark1, int englishMark2, int englishMark3)
+		{
+		}
+	}
--- BIT504/Assignment1/src/StudentManager.java
+++ BIT504/Assignment1/src/StudentManager.java
@@ -1,13 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class StudentManager
 	{
+
+	private static LinkedList<Student> students = new LinkedList<>();
+
 	public static void main(String[] args)
 		{
-		displayMenu();
-		while (selectMenuOption() != 5)
+		if(readFile("students.txt"))
 			{
 			displayMenu();
+			while (selectMenuOption() != 5)
+				{
+				displayMenu();
+				}
 			}
 		}
 
@@ -27,5 +36,57 @@ public class StudentManager
 		{
 		Scanner scanner = new Scanner(System.in);
 		return scanner.nextInt();
+		}
+
+	public static boolean readFile(String filename)
+		{
+		try
+			{
+			Scanner scanner = new Scanner(new File(filename));
+			while (scanner.hasNextLine())
+				{
+					String[] words = scanner.nextLine().split(",");
+					int id = Integer.parseInt(words[0]);
+					String firstName = words[1];
+					String lastName = words[2];
+					int mathMark1 = Integer.parseInt(words[3]);
+					int mathMark2 = Integer.parseInt(words[4]);
+					int mathMark3 = Integer.parseInt(words[5]);
+					int englishMark1 = Integer.parseInt(words[6]);
+					int englishMark2 = Integer.parseInt(words[7]);
+					int englishMark3 = Integer.parseInt(words[8]);
+					addStudent(id, firstName, lastName, mathMark1, mathMark2, mathMark3, englishMark1, englishMark2, englishMark3);
+
+				}
+				}
+		catch (FileNotFoundException e)
+			{
+			System.out.println("Failed to read file");
+			}
+			return true;
+		}
+
+	private static void addStudent(int id, String firstName, String lastName, int	mathsMark1, int mathsMark2, int mathsMark3, int englishMark1, int englishMark2,
+											 int englishMark3)
+		{
+		students.add(new Student(id, firstName, lastName, mathsMark1, mathsMark2, mathsMark3, englishMark1, englishMark2, englishMark3 ));
+		}
+
+	private static void addNewStudent()
+		{
+
+		}
+
+	private static void removeStudent()
+		{
+
+		}
+
+	private static void displayReportByMarks()
+		{
+		for (Student student: students)
+			{
+
+			}
 		}
 	}
